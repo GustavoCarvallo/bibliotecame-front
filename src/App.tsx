8069edd8fc63bf4945a11c4831a7c270aa0ddf54
Facundo Bocalandro <facundo@bocalandro.com.ar>
2020-08-25 19:36:26 -0300

    Added SideBar and TopBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TopBar from "./TopBar/TopBar";
+import SideBar from "./SideBar/SideBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <TopBar isAdmin/>
+            <SideBar isAdmin/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/TopBar/TopBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./TopBar.css";
+
+type Props = {
+    isAdmin: boolean;
+}
+
+const TopBar = (props: Props) => {
+    return(
+        <div className={"top-bar"}>
+            <div/>
+            <div className={"top-bar-title"}>Bibliotecame</div>
+            <div className={"vertical-divider"}/>
+            <div className={"top-bar-right"}>
+                <i className="far fa-user-circle user-circle-regular"/>
+                <div className={"top-bar-user-name"}>
+                    Juan Ignacio Rodriguez
+                    {props.isAdmin && <div>Administrador/a</div>}
+                </div>
+                <i className="fas fa-sign-out-alt sign-out-alt-solid"/>
+            </div>
+        </div>
+    )
+}
+
+export default TopBar;

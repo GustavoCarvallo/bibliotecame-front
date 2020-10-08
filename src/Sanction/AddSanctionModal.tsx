499306ec2efc568f367c790d00fb4845e3df5fc0
Rochi Ferreiro <43883272+rocioferreiro@users.noreply.github.com>
2020-10-08 20:23:24 -0300

    Update AddSanctionModal.tsx

--- src/Sanction/AddSanctionModal.tsx
+++ src/Sanction/AddSanctionModal.tsx
@@ -38,9 +38,9 @@ const AddSanctionModal = (props: Props) => {
             props.onSuccess("Se ha sancionado al alumno/a exitosamente!")
         })
             .catch(err => {
-                if(err.status === EXPECTATION_FAILED) props.onError("Fecha invalidad, debe ser posterior al dia de hoy y no mas de 3 meses desde ahora.")
+                if(err.status === EXPECTATION_FAILED) props.onError("Fecha invalida, debe ser posterior al dia de hoy y no mas de 3 meses desde ahora.")
                 else if(err.status === UNPROCESSABLE_ENTITY) props.onError("El alumno ya tiene una sancion activa.")
-                else if(err.status === BAD_REQUEST) props.onError("El email no fue reconosido por el sistema.")
+                else if(err.status === BAD_REQUEST) props.onError("El email no fue reconocido por el sistema.")
                 else props.onError("Hubo un error. Intente de nuevo")
             })
         props.onClose();

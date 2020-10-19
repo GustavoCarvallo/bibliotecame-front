8035e54d62c7e50defb5d457278155df473b7b72
Kuha <43881512+kuhamaven@users.noreply.github.com>
2020-10-19 19:50:34 -0300

    fixed editProfile regex

--- src/Profile/EditProfile.tsx
+++ src/Profile/EditProfile.tsx
@@ -35,9 +35,8 @@ const initialErrors = {
 
 const EditProfile = (props: Props) => {
     const [confirmPassword, setConfirmPassword] = React.useState<string>("");
-    const regexp = new RegExp(/^(?=.*\d)(?=.*[a-zA-Z]).{7,}$/);
+    const regexp = new RegExp(^(?=.*\d)(?=.*[a-zA-Z])([a-zA-Z0-9]+){7,}$);
     const [errors, setErrors] = React.useState<Errors>({...initialErrors});
-
 
     const handleSubmit = () => {
         let newErrors = validateProfile(props.profile);

97f7eeb763b6c34febbed0909c0bfe164f57096a
rocioferreirorico <rocio.ferreirorico@ing.austral.edu.ar>
2020-10-27 19:04:13 -0300

    loan history search

--- src/LoanHistory/LoanHistory.tsx
+++ src/LoanHistory/LoanHistory.tsx
@@ -6,17 +6,19 @@ import {Loan} from "../loan/LoanScreen";
 import {get, post, put} from "../utils/http";
 import {Review} from "./ReviewModal/ReviewModal";
 import {notifyError, notifySuccess} from "../router/Routes";
+import InputWithIcon from "../common/InputWithIcon/InputWithIcon";
 
 const LoanHistory = () => {
 
     const [paginationData, setPaginationData] = React.useState<PaginationData<Loan> | undefined>(undefined);
+    const [searchFilter, setSearchFilter] = React.useState<string>("")
 
     useEffect(() => {
-        getData(0);
+        getData(0, "");
     }, [])
 
-    const getData = (page: number) => {
-        get(`loan/history?page=${page}&search=`)
+    const getData = (page: number, search: string) => {
+        get(`loan/history?page=${page}&search=${search}`)
             .then(res => {
                 setPaginationData(res);
             })
@@ -26,14 +28,19 @@ const LoanHistory = () => {
     }
 
     const changePage = (selected: number) => {
-        getData(selected);
+        getData(selected, searchFilter);
+    }
+
+    const handleFilterChange = (event: any) => {
+        getData(0, event.target.value);
+        setSearchFilter(event.target.value);
     }
 
     const createReview = (bookId: number, review: Review, callBack: () => void) => {
         post(`review/create/${bookId}`, review)
             .then(() => {
                 if (callBack) callBack();
-                getData(0);
+                getData(0, "");
                 notifySuccess('La reseña fue guardada exitosamente');
             })
             .catch(err => notifyError(err))
@@ -43,7 +50,7 @@ const LoanHistory = () => {
         put(`review/${reviewId}`, review)
             .then(() => {
                 if (callBack) callBack();
-                getData(0);
+                getData(0, "");
                 notifySuccess('La reseña fue editada exitosamente')
             })
             .catch(err => notifyError(err));
@@ -51,6 +58,13 @@ const LoanHistory = () => {
 
     return (
         <div className={"loan-history-screen"}>
+            <div className={"search-container"}>
+                <InputWithIcon icon={'fas fa-search'}
+                               value={searchFilter}
+                               onChange={handleFilterChange}
+                               placeholder={"Busque algún préstamo"}/>
+            </div>
+
             <div className={"loan-history-table-container"}>
                 <LoanHistoryTable paginationData={paginationData}
                                   changePage={changePage} createReview={createReview}
